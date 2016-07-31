397b5e3fcd79e36702d52543a519b010cc7f33a2
Gianpaulo M. Soares <jam_pow@hotmail.com>
2016-07-31 19:55:25 -0300

    An impartial commit to the report parser

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
 const iconv = require('iconv-lite');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./localStorage');
+const regx = {
+	header: /^\u0000\[[^\]]+\]/,
+	emptyLine: /^\u0000\u00013\u0000/
+};
 
 fs.readFile('msinfo-reports/CPDJO-tcp.txt', function(err, content){
 	var content = iconv.decode(content, 'win1252');
 
 	var lines = content.split('\n');
 
-	console.log(lines[5].split('\t')[2]);
+	lines.forEach(function(line){
+		
+		var emptyLine = line.match(regx.emptyLine);
+
+		if(emptyLine)
+			console.log('empty');
+			//return;
+
+		var header = line.match(regx.header);
+
+		if(header)
+			console.log(line);
+		else
+			if(line.length == 3 && line.split(/\t/).length == 1)
+				console.log(line.charCodeAt(0),line.charCodeAt(1),line.charCodeAt(2));
+				//console.log(line);
+
+	});
+
 });
 
 
